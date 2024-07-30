6e4fe75157dd4cdf53126cbf42ff92eb4f854202
Ali Ramazanov <aliasifzade@gmail.com>
2024-07-30 21:30:35 +0400

    add: multer for managing files and photos

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,14 +2,21 @@ import express from "express";
 import protect from "../middlewares/protect.js";
 import UserController from "../controllers/userController.js";
 import AuthController from "../controllers/authController.js";
+import multer from "multer";
 
 const router = express.Router();
+
+const upload = multer({ dest: "public/img/users/" });
 
 router.use(protect);
 
 router.get("/me", UserController.getMe, UserController.getUser);
 
-router.patch("/my-profile", UserController.updateMyDetails);
+router.patch(
+  "/my-profile",
+  upload.single("photo"),
+  UserController.updateMyDetails
+);
 
 router.delete("/my-profile", UserController.deleteMyProfile);
 
