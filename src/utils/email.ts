79f90c19e664f2ea8760ef72ee06a7d5e5d7ffa5
Ali Ramazanov <aliasifzade@gmail.com>
2024-08-08 15:45:46 +0400

    update: handling emails

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,29 +1,54 @@
 import nodemailer from "nodemailer";
 
-const sendEmail = async (options: any) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.emailhost as string,
-      port: Number(process.env.emailport),
+class Email {
+  private to: string;
+  private firstName: string;
+  private url: string;
+  private from: string;
+
+  constructor(user: { email: string; name: string }, url: string) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Experiences salutes You! from: <${process.env.emailfrom}>`;
+  }
+
+  private createTransport() {
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST as string,
+      port: Number(process.env.EMAIL_PORT),
       auth: {
-        user: process.env.emailname as string,
-        pass: process.env.emailpassword as string,
+        user: process.env.EMAIL_USERNAME as string,
+        pass: process.env.EMAIL_PASSWORD as string,
       },
     });
+  }
 
+  private async send(template: string, subject: string) {
     const mailOptions = {
-      from: "Experiences",
-      to: options.email,
-      subject: options.subject,
-      text: options.message,
+      from: this.from,
+      to: this.to,
+      subject,
+      text: `Hello ${this.firstName},\n\n${template}\n\nBest regards,\nExperiences Team`,
     };
 
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent: " + info.response);
-  } catch (error: any) {
-    console.error("Error sending email:", error.message);
-    throw new Error("Error sending email");
+    const transporter = this.createTransport();
+    await transporter.sendMail(mailOptions);
   }
-};
 
-export default sendEmail;
+  async sendWelcome() {
+    await this.send(
+      `Welcome to our service! We are thrilled to have you onboard. Please visit this link to activate your account: ${this.url}`,
+      "Welcome to Experiences!"
+    );
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      `We received a request to reset your password. Please visit this link to reset your password: ${this.url}`,
+      "Your password reset token (valid for 10 minutes)"
+    );
+  }
+}
+
+export default Email;
--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,7 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import multer, { FileFilterCallback } from "multer";
 import sharp from "sharp";
+import asyncErrorWrapper from "./catch";
 import ApplicationError from "./error";
+
+export interface MulterRequest extends Request {
+  user?: any;
+  file?: Express.Multer.File;
+  files?: { [fieldname: string]: Express.Multer.File[] };
+}
 
 const multerStorage = multer.memoryStorage();
 
@@ -31,55 +38,62 @@ const upload = multer({
   limits: { fileSize: 2048 * 2048 * 5 },
 });
 
-export const resizeProfilePhoto = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (!req.file) return next();
-  const userId = (req as any).user?.id || "unknown";
-  req.file.filename = `user-${userId}-${Date.now()}.jpeg`;
+export const resizeProfilePhoto = asyncErrorWrapper(
+  async (req: Request, _res: Response, next: NextFunction) => {
+    const multerReq = req as MulterRequest;
+    if (!multerReq.file) return next();
+    const userId = multerReq.user?.id || "unknown";
+    multerReq.file.filename = `user-${userId}-${Date.now()}.jpeg`;
 
-  try {
-    await sharp(req.file.buffer)
-      .resize(700, 700)
-      .toFormat("jpeg")
-      .jpeg({ quality: 95 })
-      .toFile(`public/img/users/${req.file.filename}`);
-    next();
-  } catch (err) {
-    next(err);
+    try {
+      await sharp(multerReq.file.buffer)
+        .resize(700, 700)
+        .toFormat("jpeg")
+        .jpeg({ quality: 95 })
+        .toFile(`public/img/users/${multerReq.file.filename}`);
+      next();
+    } catch (err) {
+      next(err);
+    }
   }
-};
+);
 
 export const uploadUserPhoto = upload.single("photo");
 
-export const uploadExperienceImages = upload.fields([
-  { name: "cover", maxCount: 1 },
-  { name: "images", maxCount: 3 },
-]);
+export const resizeExperienceImages = asyncErrorWrapper(
+  async (req: Request, _res: Response, next: NextFunction) => {
+    const multerReq = req as MulterRequest;
+    if (!multerReq.files?.cover?.[0] || !multerReq.files?.images?.[0])
+      return next();
 
-export const resizeExperienceImages = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (!req.file) return next();
-  const userId = (req as any).user?.id || "unknown";
-  req.file.filename = `user-${userId}-${Date.now()}.jpeg`;
+    multerReq.body.cover = `experience-${multerReq.params.id}-${Date.now()}-cover.jpeg`;
 
-  try {
-    await sharp(req.file.buffer)
-      .resize(700, 700)
-      .toFormat("jpeg")
-      .jpeg({ quality: 95 })
-      .toFile(`public/img/users/${req.file.filename}`);
-    next();
-  } catch (err) {
-    next(err);
+    try {
+      const coverFile = multerReq.files.cover[0];
+      await sharp(coverFile.buffer)
+        .resize(2000, 1400)
+        .toFormat("jpeg")
+        .jpeg({ quality: 95 })
+        .toFile(`public/img/users/${multerReq.body.cover}`);
+
+      req.body.images = [];
+
+      await Promise.all(
+        multerReq.files.images.map(async (file, index) => {
+          const imageSaveName = `experience-${multerReq.params.id}-${Date.now()}-${index + 1}.jpeg`;
+
+          await sharp(file.buffer)
+            .resize(2000, 1400)
+            .toFormat("jpeg")
+            .jpeg({ quality: 95 })
+            .toFile(`public/img/users/${imageSaveName}`);
+
+          req.body.images.push(imageSaveName);
+        })
+      );
+      next();
+    } catch (err) {
+      next(err);
+    }
   }
-};
-
-export interface MulterRequest extends Request {
-  file?: Express.Multer.File;
-}
+);
